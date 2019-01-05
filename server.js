8626d195be1c18ed624a896ed39fea29b94652ff
Rylie Johnson <paintedflame@gmail.com>
2019-01-05 13:24:40 -0600

    issues

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controller/booksController.js");
+var routes = require("./app/controller/booksController.js");
 
 app.use(routes);
 
